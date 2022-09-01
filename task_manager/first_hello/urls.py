7551137b8a3307bd350dc177ac9fd979f7d49eae
Max Odinokiy <max.odinokiy@gmail.com>
2022-09-01 19:36:14 +0300

    Add main page v1

--- task_manager/first_hello/urls.py
+++ task_manager/first_hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from task_manager.first_hello import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('first_hello/', views.first_hello),
 ]
--- task_manager/first_hello/views.py
+++ task_manager/first_hello/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'first_hello/index.html'
-    return render(request, template, context={
-        'text': 'Hello! This is my first Django app'
-    })
+    return render(request, template)
+
+
+def first_hello(request):
+    template = 'first_hello/first_hello.html'
+    return render(request, template)
