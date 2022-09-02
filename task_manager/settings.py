681daa7fff4732c2987c4b4e14dd6e140168fe1d
Max Odinokiy <max.odinokiy@gmail.com>
2022-09-02 11:06:57 +0300

    Add i18n translation

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -115,7 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LOCALE_PATHS = [Path('.')/'locale',]
+
+LANGUAGES = (
+    ("en", "English"),
+    ("ru", "Russian"),
+)
 
 TIME_ZONE = 'Europe/Moscow'
 
